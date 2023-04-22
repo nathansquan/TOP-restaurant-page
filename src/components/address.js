b5d0475f74d4bf5a9a60fd4ddf4c3ba4cd335d89
Nathan Quan <nathansquan@gmail.com>
2023-04-22 17:10:46 -0400

    implement Address class

---
+++ src/components/address.js
@@ -0,0 +1,28 @@
+class Address {
+    constructor(street, city, state, zipcode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+
+    createAddress() {
+        const addressDiv = document.createElement("div");
+        addressDiv.classList.add("address");
+
+        const addressLine1Div = document.createElement("div");
+        addressLine1Div.classList.add("address-line-1");
+        addressLine1Div.textContent = this.street;
+
+        const addressLine2Div = document.createElement("div");
+        addressLine2Div.classList.add("address-line-2");
+        addressLine2Div.textContent = `${this.city}, ${this.state} ${this.zipcode}`;
+
+        addressDiv.appendChild(addressLine1Div);
+        addressDiv.appendChild(addressLine2Div);
+
+        return addressDiv;
+    }
+}
+
+export default Address;
