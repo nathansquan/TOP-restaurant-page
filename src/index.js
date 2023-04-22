01cd285dec043291c34def6b44f93890c13efa93
Nathan Quan <nathansquan@gmail.com>
2023-04-22 17:12:06 -0400

    implement click handler to clear contents and display home or contact pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import firstLoad from './components/firstLoad.js';
+import Contact from './contact/contact.js';
 import './css/style.css';
+import loadHome from './home/home.js';
 
 firstLoad();
+
+const content = document.querySelector("#content");
+content.addEventListener("click", clickHandlerTab);
+
+function clickHandlerTab(e) {
+    // clear page contents
+    content.textContent = "";
+
+    switch (e.target.className) {
+        case 'homeBtn':
+            loadHome();
+            break;
+        case 'menuBtn':
+            break;
+        case 'contactBtn':
+            const contact = new Contact;
+            contact.loadContact();
+            break;
+        default:
+            break;
+    }
+}
